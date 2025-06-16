c85e9032e3cf5c803cf1b31b96af0ac8da5bcde4
CEO1 <christian4onos@outlook.com>
2025-06-16 02:33:21 +0100

    16th june: init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "time"
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+
+	}
+}
+
+func main() {
+
+}
